e0056653b4c2d40bb121a8b50b5b863a4123bb93
jozsef meszaros <joseph.meszaros89@yahoo.co.uk>
2017-04-24 11:24:13 +0100

    adds show-route function

--- client/src/views/map_wrapper.js
+++ client/src/views/map_wrapper.js
@@ -1,26 +1,9 @@
 var MapWrapper = function(container, coords, zoom){
 
-  this.googleMap = new google.maps.Map(container, {
-    center: coords,
-    zoom: zoom
-  })
-
-
-
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer({
-           draggable: true,
-           map: this.googleMap,
-           panel: document.getElementById('right-panel')
-         });
-  console.log(this.googleMap)
-         directionsDisplay.addListener('directions_changed', function() {
-           computeTotalDistance(directionsDisplay.getDirections());
-         });
-
-         this.displayRoute('Perth, WA', 'Sydney, NSW', directionsService,
-            directionsDisplay);
-  }
+  this.googleMap = new google.maps.Map(container, {center: coords,zoom: zoom});
+  this.directionsService = new google.maps.DirectionsService;
+  this.directionsDisplay = new google.maps.DirectionsRenderer({ draggable: true, map: this.googleMap});
+}
 
 
 
@@ -31,50 +14,84 @@ MapWrapper.prototype = {
       position: coords,
       map: this.googleMap
     })
-
-
 //    currentDirections.routes[0].legs[0].via_waypoint[]. 
-
     // var infowindow = new google.maps.InfoWindow({
     //   content: text
     // })
-
     // marker.onLoad = infowindow.open(this.googleMap,marker)
-
     // marker.addListener('click', function(){
     //   infowindow.open(this.googleMap,marker);
     // })
   },
 
-  displayRoute: function (origin, destination, service, display) {
-         service.route({
-           origin: origin,
-           destination: destination,
-           waypoints: [{location: 'Adelaide, SA'}, {location: 'Broken Hill, NSW'}],
-           travelMode: 'WALKING',
-           avoidTolls: true
-         }, function(response, status) {
-           if (status === 'OK') {
-             display.setDirections(response);
-           } else {
-             alert('Could not display directions due to: ' + status);
-           }
-         });
-       }
+    showRoute1: function (origin, destination,waypoints, service, display) {
 
-       // function computeTotalDistance(result) {
-       //   var total = 0;
-       //   var myroute = result.routes[0];
-       //   for (var i = 0; i < myroute.legs.length; i++) {
-       //     total += myroute.legs[i].distance.value;
-       //   }
-       //   total = total / 1000;
-       //   document.getElementById('total').innerHTML = total + ' km';
-       // }
+      service.route({
+        origin: origin,
+        destination: destination,
+        waypoints: waypoints,
+        travelMode: 'WALKING',
+        avoidTolls: true
+      }, function(response, status) {
+        if (status === 'OK') {
+
+          //// the route is displayed here
+          this.directionsDisplay.setDirections(response);
+
+          ///////////response is the same as the line under
+          //console.log(response.routes[0].legs[0]);
+          //console.log(this.directionsDisplay.directions.routes[0].legs[0]);
+          ///////////////////////////////////////////////////////
+
+         // console.log(routeData1.via_waypoints);
+
+          this.directionsDisplay.addListener("directions_changed", function(){
+
+            console.log("directions changed");
+            console.log(this.directionsDisplay.directions.routes[0].legs[0]);
+
+            //// create object from directions
+          var routeData = {};
+
+          var routeDirections = this.directionsDisplay.directions.routes[0].legs[0]
+
+          var routeWaypoints = [];
+
+          routeData.startpoint = {'lat': routeDirections.start_location.lat(), 'lng':routeDirections.start_location.lng()}
+
+          routeData.endpoint = {'lat': routeDirections.end_location.lat(), 'lng':routeDirections.end_location.lng()}
+
+          var viawp = routeDirections.via_waypoints
+          for(var i=0;i<viawp.length;i++){
+            routeWaypoints[i] = [viawp[i].lat(),viawp[i].lng()]
+          }
+          routeData.waypoints = routeWaypoints;
+               
+          var jsonString = JSON.stringify(routeData)
+          console.log(jsonString);
+          /////////////////////////// post
+             var request = new XMLHttpRequest();
+             request.open("POST", "http://localhost:3000/api/adventures");
+             request.setRequestHeader("Content-Type", "application/json");
+             request.onload = function(){console.log("sending");};
+             request.send(jsonString);
+           
+
+           //////post end
+
+          }.bind(this))
 
 
+        } else {
+          alert('Could not display directions due to: ' + status);
+        }
+      }.bind(this));
+            
 
-};
+    }
+
+  };
+
 
 
 module.exports = MapWrapper;
